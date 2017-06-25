82cc29befcb7720b8ace4b78c03c88cf6038f543
momojohnson <momo.johnson1987@gmail.com>
2017-06-25 09:00:22 -0500

    Fix the background not resizing

--- static/js/artist.js
+++ static/js/artist.js
@@ -5,3 +5,21 @@ $(document).ready(function() {
   	});
   });
 });
+
+$(document).ready(function() {
+  $('#slideshow').cycle({
+  fx: 'fade',
+  pager: '#smallnav',
+  pause:   1,
+  speed: 1800,
+  timeout:  3500
+  });
+});
+
+
+  $(function(){
+  $('.carousel').carousel({
+    interval: 4000
+});
+
+});
